a0644d59af9b232f606caa64266be50673d570ff
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-11-08 21:25:36 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,18 @@ import accordionData from "../data/accordionData";
 export default function Home() {
   return (
     <div>
-      <h1>Accordion Example</h1>
+      <div className="flex flex-col items-center justify-center ">
+  <h1 className="text-2xl font-bold mb-4">Accordion Example</h1>
+  <a
+    href="https://github.com/b1ink0/SPPU-BE-IT-DL-LP-IV"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-blue-600 underline"
+  >
+    GitHub
+  </a>
+</div>
+
       <Accordion data={accordionData} />
     </div>
   );
