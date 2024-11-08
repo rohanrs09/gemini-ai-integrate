85a6df4026b6b63b822ea3f6b0cfdb3489aac9d1
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-11-08 21:33:58 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center ">
   <h1 className="text-2xl font-bold mb-4">Accordion Example</h1>
   <a
-    href="https://github.com/b1ink0/SPPU-BE-IT-DL-LP-IV"
+    href="https://github.com/rohanrs09/hk.git"
     target="_blank"
     rel="noopener noreferrer"
     className="text-blue-600 underline"
